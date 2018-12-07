73fc6838e54bcda5ec51a235ce92fef609e73f2f
martellmoreno <miguelartell15@gmail.com>
2018-12-07 18:37:08 +0100

    Barrera hecha

--- schedsim/barrier.c
+++ schedsim/barrier.c
@@ -30,6 +30,14 @@ int sys_barrier_init(sys_barrier_t *barrier, unsigned int nr_threads)
 	/* Initialize fields in sys_barrier_t
 	     ... To be completed ....
 	*/
+
+	barrier->max_threads = nr_threads;
+	barrier->nr_threads_arrived = 0;
+
+	pthread_mutex_init(&(barrier->mutex),NULL);
+	pthread_cond_init(&(barrier->cond), NULL);
+
+
 	return 0;
 }
 
@@ -39,6 +47,11 @@ int sys_barrier_destroy(sys_barrier_t *barrier)
 	/* Destroy synchronization resources associated with the barrier
 	      ... To be completed ....
 	*/
+
+	pthread_mutex_destroy(&(barrier->mutex));
+	pthread_cond_destroy(&(barrier->cond));
+
+
 	return 0;
 }
 
@@ -47,16 +60,29 @@ int sys_barrier_wait(sys_barrier_t *barrier)
 {
 	/* Implementation outline:
 	   - Every thread arriving at the barrier acquires the lock and increments the nr_threads_arrived
-	    counter atomically
-	     * In the event this is not the last thread to arrive at the barrier, the thread
-	       must block in the condition variable
-	     * Otherwise...
-	        1. Reset the barrier state in preparation for the next invocation of sys_barrier_wait() and
-	        2. Wake up all threads blocked in the barrier
-	   - Don't forget to release the lock before returning from the function
+	    counter atomically*/
+	    pthread_mutex_lock(&barrier->mutex);
+	    barrier->nr_threads_arrived++;
+	     /* In the event this is not the last thread to arrive at the barrier, the thread
+	       must block in the condition variable*/
+	    if(barrier->nr_threads_arrived < barrier->max_threads)
+	    {
+	    	while(barrier->nr_threads_arrived != 0)
+	    		pthread_cond_wait(&barrier->cond,&barrier->mutex);
 
-	    ... To be completed ....
+	    }
+	     /* Otherwise...
+	        1. Reset the barrier state in preparation for the next invocation of sys_barrier_wait() and*/
+	    else{
+	    	barrier->nr_threads_arrived= 0;
+
+	       /* 2. Wake up all threads blocked in the barrier
+	   - Don't forget to release the lock before returning from the function*/
+	    	pthread_cond_broadcast(&barrier->cond);
+	    }
+	   /* ... To be completed ....
 	*/
+	    pthread_mutex_unlock(&barrier->mutex);
 	return 0;
 }
 
